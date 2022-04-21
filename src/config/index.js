be4c3ed1227074865048ddb277490e009465269c
AlexSinMas <ba.mota@iesdoctorbalmis.com>
2022-04-21 17:08:01 +0200

    prueba mal hecha [BORRAR]

--- src/config/index.js
+++ src/config/index.js
@@ -3,10 +3,10 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 module.exports = {
-  PORT: process.env.PORT,
-  MONGO_URI: process.env.MONGO_URI,
-  APPLICATION_NAME: process.env.APPLICATION_NAME,
-  JWT_SECRET: process.env.JWT_SECRET,
-  CACHE_KEY: process.env.CACHE_KEY,
-  SWAGGER_PATH: `../config/swagger/${process.env.SWAGGER_DOC}.json`
+  PORT: 5000,//process.env.PORT,
+  MONGO_URI: "mongodb+srv://AlexSinMas:1231212_@cluster0.gwz1u.mongodb.net/shareyouridea?retryWrites=true&w=majority",//process.env.MONGO_URI,
+  APPLICATION_NAME: "api-granja",//process.env.APPLICATION_NAME,
+  JWT_SECRET: "4l3x4nd3r82@_",//"process.env.JWT_SECRET",
+  //CACHE_KEY: process.env.CACHE_KEY,
+  SWAGGER_PATH: `../config/swagger/swaggerPROD.json`//`../config/swagger/${process.env.SWAGGER_DOC}.json`
 };
