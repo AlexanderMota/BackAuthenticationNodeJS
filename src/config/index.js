271751744aebf2b6dfe9f0a488f3c039860f9d04
AlexSinMas <ba.mota@iesdoctorbalmis.com>
2022-04-22 11:49:19 +0200

    error env URL no URI

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 module.exports = {
-  MONGO_URI: process.env.MONGO_URI,
+  MONGO_URI: process.env.DATABASE_URL,
   APPLICATION_NAME: process.env.APPLICATION_NAME,
   JWT_SECRET: process.env.JWT_SECRET,
   //CACHE_KEY: process.env.CACHE_KEY,
