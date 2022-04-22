1ae26ec6ba59703ceb3f4683b7841bbf7d9e2409
AlexSinMas <ba.mota@iesdoctorbalmis.com>
2022-04-22 12:40:56 +0200

    error env

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,12 @@
-if (process.env.NODE_ENV !== "production") {
+/*if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
-}
+}*/
 
 module.exports = {
   PORT : process.env.PORT,
+  WEBSITE_PORT : process.env.WEBSITE_PORT,
   MONGO_URI: process.env.DATABASE_URL,
+  MONGO_URI_DEV: process.env.DATABASE_URL_DEV,
   APPLICATION_NAME: process.env.APPLICATION_NAME,
   JWT_SECRET: process.env.JWT_SECRET,
   //CACHE_KEY: process.env.CACHE_KEY,
